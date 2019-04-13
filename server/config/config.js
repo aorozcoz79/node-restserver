78357c8b04c7c0b85953f0631fd650023df6bf90
Abrahamzick Orozco <aorozcoz79@hotmail.com>
2019-04-12 23:02:11 -0500

    Conexion a DB Mongo Atlas y DB Local

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,25 @@
 //  PUERTO 
 // ===================================
 
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 33000
+
+// ===================================
+//  ENTORNO
+// ===================================
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// ===================================
+//  ENTORNO
+// ===================================
+
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://aorozcoz79:9Mu3i2h4sUve5Fee@cluster0-rrjtr.mongodb.net/cafe?retryWrites=true'
+}
+
+process.env.URLDB = urlDB
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 require('./config/config')
 
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,34 +12,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('Get Usuario')
-})
-app.post('/usuario', function(req, res) {
-    let body = req.body
+// RUTAS
 
-    if (body.name === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            personas: body
-        })
-    }
-})
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id
-    res.json({
-        name: 'Put Usuario',
-        id
-    })
-})
-app.delete('/usuario/:id', function(req, res) {
-    let id = req.params.id
-    res.json({ nombre: 'delete Usuario', id })
-})
+app.use(require('./routes/usuario'))
+
+//CONEXION BD
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err
+
+        console.log('BD online');
+    });
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto:`, process.env.PORT);
